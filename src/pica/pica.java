12dbe3d3f5924f2c7fc1801d0195755dec761b4c
Kalejs <jegors.kalejs@sk.lvt.lv>
2023-03-15 15:49:51 +0200

    Idejas, darba plans

--- src/pica/pica.java
+++ src/pica/pica.java
@@ -2,4 +2,21 @@ package pica;
 
 public class pica {
 
+	public static void main(String[] args) {
+		//Idejas, kadas funkcijas bus programma
+		//1. Sanemt visu par pirceju pirceju (telefona numurs, vards, uzvards, adrese)
+		//2. Iespçjams konstruet picu (lielums, piedevas, mçrces)
+		//3. Cena mainas atkarigi no picas lieluma, piedavam utt.
+		//ja klients picu izvçlas izòemt uz vietas ceptuvç, tad piegâdes cena nav jâiekïauj
+		//4.Kadas ir akcijas (Piemeram perc 1 picu ar extra sieru, davana bus 1 maza pica)
+		//5.Picerijas likumi(Privacy Policy)
+		//6.Pasûtîjuma izsekoðana
+		//7. Picerijas adresi + kontakti
+		//8. Pasutijumu vesture
+		//Features: piedavat klientam dzerienu
+
+		
+		
+		
+	}
 }
